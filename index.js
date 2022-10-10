1457d3a50e964f7cb7ccfbe590094f7bc6c548f0
TecSistem <69233035+luksdev@users.noreply.github.com>
2022-10-10 20:04:03 -0300

    chore: adding use to routes instructors

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const userRouter = require("./src/routes/user.router.js");
 const classroomRouter = require("./src/routes/classroom.router.js");
 const courseRouter = require("./src/routes/course.router.js");
 const moduleRouter = require("./src/routes/module.router.js");
+const instructorRouter = require("./src/routes/instructor.router");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -23,6 +24,7 @@ app.use("/", userRouter);
 app.use("/", classroomRouter);
 app.use("/", courseRouter);
 app.use("/", moduleRouter);
+app.use("/", instructorRouter);
 
 app.listen(3001, () => {
   console.log("Server started on port 3000");
