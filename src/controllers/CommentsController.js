a166e11cba7c27b7b3d5d6f334d59d72ba1d878a
Victor-Antonelli <suporte@tecsistemconsultoria.com.br>
2022-10-10 21:13:25 -0300

    chore: added service, controller and router to comments

---
+++ src/controllers/CommentsController.js
@@ -0,0 +1,80 @@
+const CommentService = require("../services/comments.service");
+
+const getComment = (req, res) => {
+    const { id } = req.params;
+
+    CommentService.getComment(Number(id))
+    .then((comment) => {
+        if(comment){
+            res.status(200).send(comment)
+        }else{
+            res.status('404').send('Comment not found')
+        }
+
+    })
+    .catch((e) => res.status(500).send(e.message));
+} 
+
+const getCommentsByClassId = (req, res) => {
+    const { id_class } = req.params;
+    CommentService.getCommentsByClassId(Number(id_class))
+    .then((comments) => {
+        if(comments){
+            res.status(200).send(comments)
+        }else{
+            res.status(404).send('No comments found')
+        }
+    })
+    .catch((e) => res.status(500).send(e.message))
+}
+
+const saveComment = (req, res) => {
+    const { comment, date, id_class, id_user, id_comment ,usersId }  = req.body;
+
+    CommentService.createComment(
+        comment
+    ,   date
+    ,   Number(id_class)
+    ,   Number(id_user)
+    ,   Number(id_comment)
+    ,   Number(usersId)
+    )
+    .then((comment) => {
+        if(comment){
+            res.status(201).send(comment)
+        }else{
+            res.status(404).send('Não foi possível salvar o comentario')
+        }
+    })
+    .catch((e) => res.status(500).send(e.message));
+}
+
+const deleteComment = (req, res) => {
+    CommentService.deleteComment(Number(req.params.id))
+    .then((comment) => {
+        if(comment) {
+            res.status(200).send(comment)
+        }else{
+            res.status('404').send('Comment not found')
+        }
+    })
+
+}
+
+const updateComment = (req, res) => {
+    const { comment, date }  = req.body
+    CommentService.updateComment(
+        Number(req.params.id)
+    ,   comment
+    ,   date
+    )
+
+}
+
+module.exports = {
+    getComment
+,   getCommentsByClassId
+,   saveComment
+,   updateComment
+,   deleteComment
+}
---
+++ src/routes/comment.router.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const Controller = require("../controllers/CommentsController");
+
+const comment = express.Router();
+
+comment.get("/comment/:id", async (req, res) =>
+  Controller.getComment(req, res)
+);
+
+comment.get("/commentsByClass/:id_class", async (req, res) =>
+  Controller.getCommentsByClassId(req, res)
+);
+
+comment.post("/comment/add", async (req, res) =>
+  Controller.saveComment(req, res)
+);
+
+comment.delete("/comment/delete/:id", async (req, res) =>
+  Controller.deleteComment(req, res)
+);
+
+comment.put("/comment/update/:id", async (req, res) =>
+  Controller.updateComment(req, res)
+);
+
+module.exports = comment;
